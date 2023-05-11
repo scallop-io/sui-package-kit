afbede53a03301c11dac93569684e08e164ae033
icefox <zengxinhai001@163.com>
2023-05-11 11:10:37 +0800

    Feat: init the SuiAdvancePackagePublisher with networkType

--- src/lib/advance/sui-advance-package-publisher.ts
+++ src/lib/advance/sui-advance-package-publisher.ts
@@ -2,7 +2,7 @@ import { RawSigner } from "@mysten/sui.js"
 import { SuiPackagePublisher } from "../sui-package-publisher"
 import type { NetworkType } from "./network-type"
 import { publishPackageBatch, PackageBatch } from "./publish-package-batch"
-import { publishPackageEmpower } from "./publish-package-empower"
+import { publishPackageEmpower, PublishPackageOption } from "./publish-package-empower"
 
 /**
  * This is an advance version of the `SuiPackagePublisher` class
@@ -14,20 +14,22 @@ import { publishPackageEmpower } from "./publish-package-empower"
  */
 export class SuiAdvancePackagePublisher {
   public packagePublisher: SuiPackagePublisher
+  public networkType: NetworkType;
 
   /**
    * Create a `SuiPackageAdvancePublisher` instance
    * @param suiBin, the path to the `sui` binary, if not provided, it will use the `sui` in the `PATH`
+   * @param networkType the network type: `testnet` | `mainnet` | 'devnet' | 'localhost'
    */
-  constructor(suiBin?: string) {
-    this.packagePublisher = new SuiPackagePublisher(suiBin);
+  constructor(params: {suiBin?: string, networkType: NetworkType}) {
+    this.networkType = params.networkType;
+    this.packagePublisher = new SuiPackagePublisher(params.suiBin);
   }
 
   /**
    * Publish the move package and provide options to write toml and save objectIds
    * @param pkgPath path to the move package
    * @param signer the `RawSigner` from the @mysten/sui.js
-   * @param networkType the network type: `testnet` | `mainnet` | 'devnet' | 'localhost'
    * @param option
    *  option.enforce: if true, the package will be published even if it's already published for the networkType
    *  option.writeToml: if true, it will write a `Move.${networkType}.toml` file for the package
@@ -35,17 +37,16 @@ export class SuiAdvancePackagePublisher {
    *
    *  @return the `PackagePublishResult` from the `SuiPackagePublisher` or `undefined` if the package is already published
    */
-  public async publishPackage(pkgPath: string, signer: RawSigner, networkType: NetworkType) {
-    return await publishPackageEmpower(this.packagePublisher, pkgPath, signer, networkType);
+  public async publishPackage(pkgPath: string, signer: RawSigner, option?: PublishPackageOption) {
+    return await publishPackageEmpower(this.packagePublisher, pkgPath, signer, this.networkType, option);
   }
 
   /**
    * Publish a batch of packages in order
    * @param packageBatch the array of packages to publish, you can specify the option for each package
    * @param signer the `RawSigner` from the @mysten/sui.js
-   * @param networkType the network type: `testnet` | `mainnet` | 'devnet' | 'localhost'
    */
-  public async publishPackageBatch(packageBatch: PackageBatch, signer: RawSigner, networkType: NetworkType) {
-    return await publishPackageBatch(this.packagePublisher, packageBatch, signer, networkType);
+  public async publishPackageBatch(packageBatch: PackageBatch, signer: RawSigner) {
+    return await publishPackageBatch(this.packagePublisher, packageBatch, signer, this.networkType);
   }
 }
