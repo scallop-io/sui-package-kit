f86d6c9bf09949cc40deac59914408eaa1429471
icefox <zengxinhai001@163.com>
2023-07-19 10:13:20 +0800

    Update the dependencies

--- src/lib/build-package.ts
+++ src/lib/build-package.ts
@@ -44,7 +44,8 @@ export const buildPackage = (suiBinPath: string, packagePath: string, options: B
       `${suiBinPath} move build --dump-bytecode-as-base64 --path ${packagePath} ${skipDepFetch} ${withUnpublishedDep}`;
     console.log('Running build package command')
     console.log(buildCmd.cyan.bold)
-    const buildCommandOutput = execSync(`${buildCmd} --install-dir ${tmpDir.name}`, {encoding: 'utf-8'});
+    // pass in stdio: inherit to show the output of the build command, now it's pipe
+    const buildCommandOutput = execSync(`${buildCmd} --install-dir ${tmpDir.name}`, {encoding: 'utf-8', stdio: 'pipe' });
     const { modules, dependencies, digest } = JSON.parse(buildCommandOutput);
     console.log('Build package success'.green)
     return {
